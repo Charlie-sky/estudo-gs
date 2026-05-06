8b97be8815518a97bf9d4c6634e5986b74c77a80
Charlie-sky <caiodeoliveira951@gmail.com>
2026-05-06 20:08:04 -0300

    Create exec7.js

---
+++ exec7.js
@@ -0,0 +1,8 @@
+let carro = {
+    modelo: "Civic",
+    ano: 2020,
+    Automatico: true
+};
+
+
+console.log(`O carro modelo ${carro.modelo} do ano ${carro.ano} é automatico? ${carro.Automatico} `)
