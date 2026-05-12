aab5e6970c1be66792f74a26cdfa76438394d07a
Caio-Sky <caiodeoliveira951@gmail.com>
2026-05-12 13:31:18 -0300

    updade quase finau

---
+++ exec8.js
@@ -0,0 +1,5 @@
+let linguagens = ["Python", "Javascript", "Fluter"]
+console.log("Linguagens favoritas:")
+console.log("- " +linguagens[0])
+console.log("- " +linguagens[1])
+console.log("- " +linguagens[2])
---
+++ exec9.js
@@ -0,0 +1,9 @@
+let contador = 6
+
+
+while (contador >= 1) {
+    contador -= 1;
+    console.log("Contagem regresiva", contador);
+    if (contador===1);
+}
+console.log("Fim de contagem")
